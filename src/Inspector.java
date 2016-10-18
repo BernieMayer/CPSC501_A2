f9e8ba576fbf3d8d6b0fc50a5e2c32537ebe4726
Bernie Mayer <bemayer@ucalgary.ca>
2016-10-18 15:16:50 -0600

    Added some code to the Inspector class

--- src/Inspector.java
+++ src/Inspector.java
@@ -1,9 +1,31 @@
 
+import java.lang.reflect.*;
+
 public class Inspector {
 
 	public void inspect(Object obj, boolean recursive)
 	{
 		
 	}
+	
+	
+	/**
+	 * 
+	 * @param o
+	 */
+		
+	public void printFieldNames(Object o) {
+		Class c = o.getClass();
+		Field[] publicFields = c.getFields();
+		
+		for (int i = 0; i < publicFields.length; i++){
+			String fieldName = publicFields[i].getName();
+			Class typeClass = publicFields[i].getType();
+			String fieldType = typeClass.getName();
+			
+		
+		}
+		
+}
 	
 }
