eba156ba1da32de502a5b7d684d2aae6e453f11e
Bernie Mayer <bemayer@ucalgary.ca>
2016-10-24 14:23:39 -0600

    Adding some code to the Inspector

--- src/Inspector.java
+++ src/Inspector.java
@@ -2,7 +2,12 @@
 import java.lang.reflect.*;
 
 public class Inspector {
-
+	
+	public Inspector()
+	{
+		
+	}
+	
 	public void inspect(Object obj, boolean recursive)
 	{
 		
@@ -22,10 +27,36 @@ public class Inspector {
 			String fieldName = publicFields[i].getName();
 			Class typeClass = publicFields[i].getType();
 			String fieldType = typeClass.getName();
-			
-		
 		}
 		
-}
+  }
+	
+  public String getDeclaringClassName(Class classObject)
+  {
+	  String className = classObject.getName();
+	  return className;
+  }
+  
+  public String getSuperClassName(Class classObject)
+  {
+	  String superName = classObject.getSuperclass().getName();
+	  return superName;
+  }
+  
+  public String queryInterfaces(Class classObject)
+  {
+	  Class[] interfaces = classObject.getInterfaces();
+	  String interfaceAsString = "";
+	  for (Class a_interface:interfaces)
+	  {
+		  interfaceAsString = interfaceAsString + a_interface.getName();
+		  interfaceAsString = interfaceAsString + "\n";
+		  
+	  }
+	  
+	  return interfaceAsString;
+  }
+  
+  
 	
 }
